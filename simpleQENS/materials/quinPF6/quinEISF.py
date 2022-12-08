47b64f82495c4a9d3fd96b1e251f73340aac31cd
bernetmeijer <b.e.meijer@qmul.ac.uk>
2022-12-08 18:02:21 +0000

    put all EISFs in one function

--- simpleQENS/materials/quinPF6/quinEISF.py
+++ simpleQENS/materials/quinPF6/quinEISF.py
@@ -61,10 +61,10 @@ def amplitude(Q, r):
     return np.array([ampl1, ampl2, ampl3, ampl4, ampl5])
 
 
-def EISF_octa(Q, jump_distances, modelname='C4C2', f=1.0, m=1.0):
+def EISF_octa(Q, jump_distances, modelname='octa C4C2', f=1.0, m=1.0):
     """ Make the EISF for a given model in the octahedral point group
     for the Qvalues Q and list of 16 jump distances for each H-atom
-    Allowed modelnames are ['C4C2', 'onlyC2', 'C3C2'].
+    Allowed modelnames are ['octa C4C2', 'octa only C2', 'octa C3C2'].
     See jupyter notebook or Bee p. 225 for descriptions of these models.
     (models A, C, B respectively.)
     """
@@ -72,13 +72,14 @@ def EISF_octa(Q, jump_distances, modelname='C4C2', f=1.0, m=1.0):
     all_atom_eisfs = []
     for atom_jumps in jump_distances:  # loop over H atoms
         amplitudes = amplitude(Q, atom_jumps)
-        if modelname == 'C4C2':
+        #print('amplitudes: {}'.format(amplitudes))
+        if modelname == 'octa C4C2':
             eisf = amplitudes[0]
 
-        elif modelname == 'onlyC2':
+        elif modelname == 'octa onlyC2':
             eisf = amplitudes[0] + amplitudes[1] + amplitudes[2]
 
-        elif modelname == 'C3C2':
+        elif modelname == 'octa C3C2':
             eisf = amplitudes[0] + amplitudes[1]
 
         else:
@@ -87,8 +88,53 @@ def EISF_octa(Q, jump_distances, modelname='C4C2', f=1.0, m=1.0):
 
         all_atom_eisfs.append(eisf)
 
+    #print('all_atom_eisfs: {}'.format(all_atom_eisfs))
     total_eisf = np.average(all_atom_eisfs, axis=0)
 
     return 1-f + f*m*total_eisf
+
+
+def EISF(Q, modelname, f=1.0, m=1.0, jump_distances=None):
+    """
+    optional model names:
+    ['isotropic', 'D3 including C2', 'D3 only C3', 'octa C4C2', 'octa only C2', 'octa C3C2']
+    if you take a octa model, you need to specify the jump distances.
+    """
+
+    if modelname == 'isotropic':
+        radius = 3.23
+        return 1 - f + f * m * (spherical_jn(0, Q * radius)) ** 2
+
+    elif 'D3' in modelname:
+
+        d_C2a1 = 3.2  # 4 H
+        d_C2a2 = 4.6  # 4 H
+        d_C2b = 2.714  # 4 H
+        d_C2c = 4.515  # 2 H
+        d_C3 = 3.5  # 12 H
+
+        # sum over the C2 rotations (3 equivalent rotations)
+        sum_C2 = 3 * 1.0 / 14 * (4 * spherical_jn(0, Q * d_C2a1) + 4 * spherical_jn(0, Q * d_C2a2) +
+                                 4 * spherical_jn(0, Q * d_C2b) + 2 * spherical_jn(0, Q * d_C2c))
+
+        # sum over the C3 rotations (2 equivalent rotations)
+        sum_C3 = 2 * 1.0 / 14 * (12 * spherical_jn(0, Q * d_C3) + 2)  # the +2 comes from the 2 stationary H atoms
+
+        A_A1 = 1.0 / 6 * (1 + sum_C3 + sum_C2)
+        A_A2 = 1.0 / 6 * (1 + sum_C3 - sum_C2)
+        A_E = 1.0 / 6 * (2 - sum_C3)
+
+        if modelname == 'D3 including C2':
+            eisf_C2_present = A_A1
+            return 1 - f + f * m * eisf_C2_present
+
+        elif modelname == 'D3 only C3':
+            eisf_only_C3 = A_A1 + A_A2
+            return 1 - f + f * m * eisf_only_C3
+
+    elif 'octa' in modelname:
+        return EISF_octa(Q, jump_distances, modelname=modelname, f=f, m=m)
+
+
 
 
